e5a0ff8e639083364ace83bf68162e627e6fb7fb
Eri <efwinckler@gmail.com>
2021-12-09 21:35:38 -0500

    progress done class 12-9

--- src/main/java/com/geekylikes/app/security/WebSecurityConfig.java
+++ src/main/java/com/geekylikes/app/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.geekylikes.app.security;
 
+import com.geekylikes.app.security.jwt.AuthTokenFilter;
 import com.geekylikes.app.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
